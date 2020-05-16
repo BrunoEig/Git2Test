89d2ffa8ee8ad4182ab64d326dde119787de3325
Bruno Eigenmann <bruno-eigenmann@outlook.com>
2020-05-16 23:21:45 +0200

    Initial Commit

---
+++ Git2Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git2Test
+//
+//  Created by Bruno Eigenmann on 16.05.20.
+//  Copyright © 2020 Bruno Eigenmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
